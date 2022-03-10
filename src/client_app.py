277dc80fdb8f04b27dc72cb3b450c8f4edcbfe20
Marcin Kucharski <dixone23@gmail.com>
2022-03-10 20:35:13 +0100

    Added LICENSE

---
+++ src/client_app.py
@@ -0,0 +1,58 @@
+import socket
+import threading
+import sys
+
+
+class Client:
+    def __init__(self, server_ip, server_port) -> None:
+        self.server_ip = server_ip
+        self.server_port = server_port
+
+    def message_handler(self, connection: socket.socket) -> None:
+        while True:
+            try:
+                # Listen for incoming messages
+                message = connection.recv(1024)
+
+                # If message is received then decode it
+                # Else close connection because there could be an error
+                # or there is no message at all
+                if message:
+                    sys.stdout.write(f"{message.decode()}\n")
+                else:
+                    conn.close()
+                    raise "No message received or error while receiving message"
+                    break
+
+            except Exception as e:
+                sys.stdout.write(f"{e}\n")
+                break
+
+
+    def client_handler(self) -> None:
+        # Initialization of client socket
+        # Trying to connect with host server
+        try:
+            sock = socket.socket()
+            sock.connect((self.server_ip, self.server_port))
+
+            threading.Thread(target=self.message_handler, args=[sock]).start()
+
+            sys.stdout.write(f"Connected with: {self.server_ip}:{self.server_port}\n")
+
+            while True:
+                message = input()
+
+                if message == "/quit":
+                    print("Quitting")
+                    break
+
+                sock.send(message.encode())
+
+        except Exception as e:
+            sys.stdout.write(f"{e}\n")
+            sock.close()
+
+if __name__ == "__main__":
+    t = Client("127.0.0.1", 12000)
+    t.client_handler()
--- src/server_app.py
+++ src/server_app.py
@@ -30,7 +30,7 @@ class Server:
                 GLOB_CONNECTIONS.append(sock_conn)
 
                 # Start new thread (instance) for incoming connection
-                threading.Thread(target=conn_handler, args=[sock_conn, address]).start()
+                threading.Thread(target=self.conn_handler, args=[sock_conn, address]).start()
 
         # Handle errors
         except Exception as e:
@@ -41,9 +41,43 @@ class Server:
         finally:
             if len(GLOB_CONNECTIONS) > 0:
                 for conn in GLOB_CONNECTIONS:
-                    del_conn(conn)
+                    self.terminate_connection(conn)
 
             sock.close()
+
+    def conn_handler(self, connection: socket.socket, address: str) -> None:
+        while True:
+            try:
+                message = connection.recv(1024)
+
+                if message:
+                    sys.stdout.write(f"[{address[0]}:{address[1]}]: {message.decode()}\n")
+                    message_to_broadcast = f"Recv from [{address[0]}:{address[1]}]: {message.decode()}"
+
+                    self.broadcast_message(message_to_broadcast, connection)
+
+                else:
+                    self.terminate_connection(connection)
+
+            except Exception as e:
+                sys.stdout.write(f"{e}\n")
+                self.terminate_connection(connection)
+                break
+
+    def broadcast_message(self, message: str, connection: socket.socket) -> None:
+        for client in GLOB_CONNECTIONS:
+            if client != connection:
+                try:
+                    client.send(message.encode())
+                except Exception as e:
+                    sys.stdout.write(f"{e}\n")
+                    self.terminate_connection()
+
+
+    def terminate_connection(self, connection: socket.socket) -> None:
+        if connection in GLOB_CONNECTIONS:
+            connection.close()
+            GLOB_CONNECTIONS.remove(connection)
 
 
 if __name__ == "__main__":
